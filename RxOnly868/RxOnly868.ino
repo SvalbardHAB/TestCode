dc829e777c8cabf2037462e1494e75fe2ac41827
David Shah <davey1576@gmail.com>
2016-08-30 19:57:48 +0100

    Tweak LoRa config and Python script for long range camera

--- RxOnly868/RxOnly868.ino
+++ RxOnly868/RxOnly868.ino
@@ -1,14 +1,14 @@
 #include <SPI.h>
 
 #include <RFM98W_library.h>
-RFMLib radio =RFMLib(PB12,PC6,16,21);
+RFMLib radio = RFMLib(PB12, PC6, 16, 21);
 #define nss PB12
-void setup(){
-    Serial.begin(115200);
-       
+void setup() {
+  Serial.begin(115200);
+
   SPI.setModule(2);
   SPI.begin();
-  byte my_config[6] = {0x84,0xB4,0x88,0xAC,0xCD, 0x08};
+  byte my_config[6] = {0x84, 0x84, 0x85, 0xAC, 0xCD, 0x00};
   radio.high_frequency = true;
   radio.configure(my_config);
   radio.setFrequency(869525000);
@@ -16,33 +16,31 @@ void setup(){
   Serial.println(radio.getVersion(), HEX);
 }
 
-void RFMISR(){
- // Serial.println("interrupt");
- radio.rfm_done = true; 
+void RFMISR() {
+  // Serial.println("interrupt");
+  radio.rfm_done = true;
 }
 
-void loop(){
-  if(radio.rfm_status == 0){
-    radio.beginRX(); 
-    attachInterrupt(PC6,RFMISR,RISING);
+void loop() {
+  if (radio.rfm_status == 0) {
+    radio.beginRX();
+    attachInterrupt(PC6, RFMISR, RISING);
   }
 
-  if(radio.rfm_done){
-    Serial.println("Ending");   
+  if (radio.rfm_done) {
+    // Serial.println("Ending");
     RFMLib::Packet rx;
     radio.endRX(rx);
-    Serial.println("====++++==== packet ====++++====");
+    Serial.println("=packet=");
     Serial.print("RSSI:");
     Serial.println(rx.rssi);
     Serial.print("SNR:");
     Serial.println(rx.snr);
-   for(byte i = 0;i<rx.len;i++){
-     Serial.print(rx.data[i]);
-     Serial.print("  ") ;
-   }
-   
-   Serial.println();
+    for (byte i = 0; i < rx.len; i++) {
+      Serial.print(rx.data[i]);
+      Serial.print("  ");
+    }
+
+    Serial.println();
   }
 }
-
-
--- rx.py
+++ rx.py
@@ -1,8 +1,9 @@
-import serial,  sys
+import serial, sys, time
 curFile = -1
 f = None
-ser = serial.Serial('/dev/ttyUSB0',115200,timeout=1800000)
+ser = serial.Serial('/dev/ttyACM0',115200,timeout=1800000)
 curPacket = -1
+fileTimestamp = ""
 while True:
     line = ser.readline().decode("utf-8")
     splitline = line.split("  ")
@@ -12,19 +13,21 @@ while True:
         fileno = (packet[0] << 8) | packet[1]
         packetid = (packet[2] << 8) | packet[3]
 
-        print("File " + str(fileno) + ".jpg, packet seq " + str(packetid) + ", len " + str(len(packet)))
+        print("File " + fileTimestamp + "_" + str(fileno) + ".jpg, packet seq " + str(packetid) + ", len " + str(len(packet)))
 
         if fileno != curFile:
             if curFile != -1:
-                print("All done, saved to " + str(curFile) + ".jpg")
+                print("All done, saved to " + fileTimestamp + "_" + str(curFile) + ".jpg")
                 f.close()
                 if fileno != (curFile + 1):
                     if fileno == 0:
                         print("Warning: tx reset")
                     else:
                         print("File number did not increase sequentially - likely bad packet or tx failure")
-                        sys.exit(1)
-            f = open(str(fileno) + ".jpg","wb")
+                        #sys.exit(1)
+                        continue
+            fileTimestamp = time.strftime("%Y%m%d_%H%M%S")
+            f = open(fileTimestamp + "_" + str(fileno) + ".jpg","wb")
             curPacket = -1
         curFile = fileno
         if (packetid != curPacket + 1):
