50ab217f90c6a20588f31653e78d1469a38e33ca
David Shah <davey1576@gmail.com>
2016-08-30 19:42:24 +0100

    Tweak RX code for new STM32 board and camera support

--- RxOnly868/RxOnly868.ino
+++ RxOnly868/RxOnly868.ino
@@ -1,12 +1,14 @@
 #include <SPI.h>
 
 #include <RFM98W_library.h>
-RFMLib radio =RFMLib(20,7,16,21);
-#define nss 20
+RFMLib radio =RFMLib(PB12,PC6,16,21);
+#define nss PB12
 void setup(){
+    Serial.begin(115200);
+       
+  SPI.setModule(2);
   SPI.begin();
-  Serial.begin(38400);
-  byte my_config[6] = {0x84,0x84,0x88,0xAC,0xCD, 0x08};
+  byte my_config[6] = {0x84,0xB4,0x88,0xAC,0xCD, 0x08};
   radio.high_frequency = true;
   radio.configure(my_config);
   radio.setFrequency(869525000);
@@ -14,34 +16,33 @@ void setup(){
   Serial.println(radio.getVersion(), HEX);
 }
 
+void RFMISR(){
+ // Serial.println("interrupt");
+ radio.rfm_done = true; 
+}
+
 void loop(){
   if(radio.rfm_status == 0){
     radio.beginRX(); 
-    attachInterrupt(7,RFMISR,RISING);
+    attachInterrupt(PC6,RFMISR,RISING);
   }
 
   if(radio.rfm_done){
-        Serial.println("Ending");   
+    Serial.println("Ending");   
     RFMLib::Packet rx;
     radio.endRX(rx);
+    Serial.println("====++++==== packet ====++++====");
+    Serial.print("RSSI:");
+    Serial.println(rx.rssi);
+    Serial.print("SNR:");
+    Serial.println(rx.snr);
    for(byte i = 0;i<rx.len;i++){
      Serial.print(rx.data[i]);
-     Serial.print("  ");
+     Serial.print("  ") ;
    }
+   
    Serial.println();
-   Serial.print("RSSI = ");
-   Serial.print(rx.rssi);
-   Serial.println("dBm");
-   Serial.print("SNR = ");
-   Serial.print(rx.snr);
-   Serial.println("dB");
   }
-  
-}
-
-void RFMISR(){
-  Serial.println("interrupt");
- radio.rfm_done = true; 
 }
 
 
